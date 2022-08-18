ec0d471f9f5e0eee705ca01ffb2aed2dd8dbde08
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-18 02:33:43 -0400

    simple counter

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,44 @@
 
 use yew::prelude::*;
 
+enum Msg {
+    ButtonPress
+}
+
+struct Root {
+    counter: i32
+}
+
+impl Component for Root {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Root { counter: 0 }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::ButtonPress => {
+                self.counter += 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+        html! {
+            <div>
+                <p>{format!("counter {}", self.counter)}</p>
+                <button onclick={link.callback(|_| Msg::ButtonPress)}>{"+1"}</button>
+            </div>
+        } 
+    }
+}
+
 fn main() {
     println!("こんにちは世界！");
+
+    yew::start_app::<Root>();
 }
